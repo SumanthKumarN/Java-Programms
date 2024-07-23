dad2c9e9da46ec4aa7971d9eb4ce776098154c7c
SumanthKumarN <sumanthkumarn2002@gmail.com>
2024-07-23 11:17:52 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,40 @@
+package Methods;
+
+public class Calculator 
+{
+	
+ public static void add(int a,int b) 
+ { 
+	int result = a+b;
+	System.out.println(result);
+ }
+ 
+ public static void sub(int a,int b) 
+ { 
+	int result = a-b;
+	System.out.println(result);
+}
+ 
+ public static void mul(int a,int b) 
+ { 
+	int result = a*b;
+	System.out.println(result);
+ }
+ 
+ public static void div(int a,int b) 
+ { 
+	int result = a/b;
+	System.out.println(result);
+ }
+ 
+ public static void main(String[] args) 
+ {
+add(10,10);
+sub(10,10);
+mul(10,10);
+div(10,10);
+
+ }
+ 
+ 
+}
